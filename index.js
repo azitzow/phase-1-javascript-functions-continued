76afee6a6eb1b5499170de09a21d20f2b5fb2eac
Ana Zitzow <anasempai89@gmail.com>
2022-04-27 08:55:27 -0700

    commit tree 523f21faa25f7bf5064011f5e79183914f9777d8

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+const saturdayFun = (param) => {
+  if (param !== undefined) {
+    return `This Saturday, I want to ${param}!`;
+  }
+  return 'This Saturday, I want to roller-skate!';
+}
+
+const mondayWork = (param) => {
+  if (param !== undefined) {
+    return `This Monday, I will ${param}.`;
+  }
+  return 'This Monday, I will go to the office.';
+}
+
+const wrapAdjective = (flair = '*') => {
+  return (str = 'special') => {
+    return `You are ${flair}${str}${flair}!`;
+  }
+}
